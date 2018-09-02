cd5b5e36589476e02ff741ec6411ca4bd8d3cefd
Keos99 <keos99@bk.ru>
2018-09-02 22:56:11 +0300

    Добавлено подключение к БД Postgresql. Добавлена страница по созданию тестовой БД.

--- src/main/java/GeekToDoList/controller/AppController.java
+++ src/main/java/GeekToDoList/controller/AppController.java
@@ -1,5 +1,7 @@
 package GeekToDoList.controller;
 
+import GeekToDoList.model.CreateTable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AppController {
 
+    @Autowired
+    private CreateTable createTable;
+
     @RequestMapping("/")
     public String javaPageHello(Model model){
         model.addAttribute("name","Barsik");
         return "hello";
     }
+
+    @RequestMapping("/create")
+    public String createTable(Model model) {
+        model.addAttribute("status", createTable.createStatus());
+        // hello.jsp
+        return "table";
+    }
+
 
 }
